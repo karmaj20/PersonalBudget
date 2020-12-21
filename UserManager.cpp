1c4c86609400d054a9b08ea22079c3fee383cf7c
Karol Majchrzak <kmajchrzak1999@gmail.com>
2020-12-21 21:09:07 +0100

    Added getting data of new user

--- UserManager.cpp
+++ UserManager.cpp
@@ -1,1 +1,47 @@
 #include "UserManager.h"
+
+User UserManager::getNewUserData()
+{
+	User user;
+
+	user.setUserId(loadNewUserId());
+
+	string login = "", password = "", name = "", surname = "";
+	do {
+		cout << "What is your login ?";
+		cin >> login;
+		user.setLogin(login);
+	} while (isLoginExist(user.getLogin()) == true);
+
+	cout << "What is your password ?";
+	cin >> password;
+	user.setPassword(password);
+
+	cout << "What is your name ? ";
+	cin >> name;
+	user.setName(name);
+
+	cout << "What is your surname ?";
+	cin >> surname;
+	user.setSurname(surname);
+
+	return user;
+}
+
+int UserManager::loadNewUserId() {
+	if (users.empty() == true)
+		return 1;
+	else
+		return users.back().getId() + 1;
+}
+
+bool UserManager::isLoginExist(string login)
+{
+	for (int i = 0; i < users.size(); i++) {
+		if (users[i].getLogin() == login) {
+			cout << "Login is taken." << endl;
+			return true;
+		}
+	}
+	return false;
+}
--- UserManager.h
+++ UserManager.h
@@ -1,11 +1,17 @@
 #ifndef USERMANAGER_H
 #define USERMANAGER_H
 #include <iostream>
+#include <vector>
+
+#include "User.h"
 
 using namespace std;
 
 class UserManager {
-
+	vector<User> users;
+	User getNewUserData();
+	int loadNewUserId();
+	bool isLoginExist(string login);
 };
 
 #endif
