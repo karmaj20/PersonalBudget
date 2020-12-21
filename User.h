b24e9104d2a9dc310254b1d9ec717c7f2372dc95
Karol Majchrzak <kmajchrzak1999@gmail.com>
2020-12-21 17:47:52 +0100

    Class user Setters and getters

--- User.h
+++ User.h
@@ -5,7 +5,24 @@
 using namespace std;
 
 class User {
+	int userId;
+	string login;
+	string password;
+	string name;
+	string surname;
 
+public:
+	void setId(int newId);
+	void setLogin(string newLogin);
+	void setPassword(string newPassword);
+	void setName(string newName);
+	void setSurname(string newSurname);
+
+	int getId();
+	string getLogin();
+	string getPassword();
+	string getName();
+	string getSurname();
 };
 
 #endif
