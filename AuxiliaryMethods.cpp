d9318d4e21d99d34977bf9132d0ee90f72c6023c
Karol Majchrzak <kmajchrzak1999@gmail.com>
2020-12-22 00:36:24 +0100

    Added some auxiliary methods

--- AuxiliaryMethods.cpp
+++ AuxiliaryMethods.cpp
@@ -1,1 +1,62 @@
 #include "AuxiliaryMethods.h"
+
+string AuxiliaryMethods::intToStringConverter(int number)
+{
+	ostringstream ss;
+	ss << number;
+	string str = ss.str();
+
+	return str;
+}
+
+int AuxiliaryMethods::stringToIntConverter(string number)
+{
+	int intNumber;
+	istringstream iss(number);
+	iss >> intNumber;
+
+	return intNumber;
+}
+
+string AuxiliaryMethods::loadLine()
+{
+	string entry = "";
+	getline(cin >> ws, entry);
+	
+	return entry;
+}
+
+char AuxiliaryMethods::loadChar()
+{
+	string entry = "";
+	char sign = { 0 };
+
+	while (true){
+		getline(cin >> ws, entry);
+
+		if (entry.length() == 1) {
+			sign = entry[0];
+			break;
+		}
+		cout << "It is not single sign. Repeat." << endl;
+	}
+	
+	return sign;
+}
+
+int AuxiliaryMethods::loadInteger()
+{
+	string entry = "";
+	int number;
+
+	while (true) {
+		getline(cin >> ws, entry);
+
+		stringstream myStream(entry);
+		if (myStream >> number)
+			break;
+		cout << "It is not number. Repeat." << endl;
+	}
+
+	return number;
+}
--- AuxiliaryMethods.h
+++ AuxiliaryMethods.h
@@ -1,11 +1,17 @@
 #ifndef AUXILIARYMETHODS_H
 #define AUXILIARYMETHODS_H
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 class AuxiliaryMethods {
-
+public:
+	static string intToStringConverter(int number);
+	static int stringToIntConverter(string number);
+	static string loadLine();
+	static char loadChar();
+	static int loadInteger();
 };
 
 #endif
